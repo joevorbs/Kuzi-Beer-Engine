40b2df02c3aeb52e4926d58ac4ddb286d36c104d
joevorbs <jvorbeck1@gmail.com>
2019-04-01 10:42:17 -0400

    take all user input and output beer recs in JSON

--- iOS_Function.py
+++ iOS_Function.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[19]:
+# In[1]:
 
 
 import pandas as pd
@@ -11,21 +11,21 @@ from sklearn.externals import joblib
 from collections import defaultdict
 
 
-# In[20]:
+# In[2]:
 
 
 #Read processed csv
 beer3 = pd.read_csv('C:/Users/vorbej1/Beer-Engine/beer3.csv')
 
 
-# In[21]:
+# In[3]:
 
 
 #Convert columns to appropriate format
 beer3[['userId','beer_beerid']] = beer3[['userId', 'beer_beerid']].apply(lambda x: x.astype(str))
 
 
-# In[22]:
+# In[4]:
 
 
 #Create and prepare training set for model input
@@ -34,7 +34,7 @@ training_set = Dataset.load_from_df(beer3[['userId', 'beer_beerid', 'review_over
 training_set = training_set.build_full_trainset()
 
 
-# In[23]:
+# In[5]:
 
 
 #Set model parameters - kNN & SVD
@@ -47,7 +47,7 @@ knn = KNNBasic(sim_options=sim_options, k=10)
 svd = SVD()
 
 
-# In[24]:
+# In[14]:
 
 
 #Train model
@@ -55,31 +55,30 @@ svd = SVD()
 svd.fit(training_set)
 
 
-# In[33]:
+# In[12]:
 
 
 #Save Model
 joblib.dump(svd, 'recommender_model')
 
 
-# In[31]:
+# In[8]:
 
 
 #Load Model for API
 svd_iOS = joblib.load('recommender_model')
 
 
-# In[27]:
+# In[9]:
 
 
-#Function to accept user input and recommened new craft beers - user input to be 3 inputs
+#Function to accept user input and recommened new craft beers
 def user_input():
     input_test = pd.DataFrame(pd.read_json('[{"userId": "101010", "beer_name": "Humulus Lager"}]')) #JSON input from user
-    input_test['beer_beerid'] = pd.DataFrame(beer3.loc[beer3['beer_name'] == 'Humulus Lager', 'beer_beerid'].unique()) #Obtain beer id for beer name given by user
-    input_test['userId'] = input_test['userId'].astype(str) #Convert userId to appropriate format
-    frame = beer3.append(input_test[input_test['userId'][:0]], sort=True) #Append info to dataframe of all beer reviews
-    
-    
+    input_test['beer_beerid'] = pd.DataFrame(beer3.loc[beer3['beer_name'].isin(input_test['beer_name']), 'beer_beerid'].unique()) #Obtain beer id for beer name given by user
+    input_test['userId'] = input_test['userId'].astype(str) #Convert userId column to appropriate format for append
+    frame = beer3.append(input_test, sort=True) #Append info to dataframe of all beer reviews 
+
     frame[['userId','beer_beerid']] = frame[['userId', 'beer_beerid']].apply(lambda x: x.astype(str)) #Convert columns to appropriate format
     frame['review_overall'] = frame['review_overall'].astype('float64')
     
@@ -87,24 +86,24 @@ def user_input():
     iids2 = frame.loc[frame['userId'][:0], 'beer_beerid'] #Obtain list of ids that user has rated
     iids_to_pred = np.setdiff1d(iids,iids2) #List of all beers user didn't rate
                          
-    testtest = [['101010', beer_beerid, 4.5] for beer_beerid in iids_to_pred] #Array of beers to predict for users      
+    testtest = [['user', beer_beerid, 4.5] for beer_beerid in iids_to_pred] #Array of beers to predict for users      
     predictions2 = pd.DataFrame(svd.test(testtest)) #Predict and convert to DataFrame
     
     predictions2 = predictions2.sort_values(by=['est'], ascending = False)[:5] #Obtain top 5 predictions
     predictions3 = predictions2.merge(beer3[['beer_name','beer_beerid','beer_abv','beer_style']], left_on='iid',right_on='beer_beerid').drop_duplicates(['beer_beerid']) #Join predictions to beer3 to obtain additional information
     
-    predictions3 = predictions3[['beer_name','beer_abv','beer_style']].to_dict() #Convert desired output to dictionary for iOS output
+    predictions3 = predictions3[['beer_name','beer_abv','beer_style']].to_json(orient='index') #Convert desired output to json for iOS output
     
     return predictions3
 
 
-# In[28]:
+# In[10]:
 
 
 result = user_input()
 
 
-# In[29]:
+# In[13]:
 
 
 result
