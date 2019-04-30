556b2ebdb1537a56b8862e3c40fb671c9bcbeeee
joevorbs <jvorbeck1@gmail.com>
2019-04-30 17:57:28 -0400

    added brewery name to file

--- Pre_Processing.py
+++ Pre_Processing.py
@@ -36,28 +36,28 @@ beer = beer[(beer['review_overall'] > 0) & (beer['userId'] > 1)]
 beer = beer.groupby('userId').filter(lambda x: x['userId'].count()>=3)
 
 
-# In[6]:
+# In[8]:
 
 
 #Select relevant columns
-beer = beer[['userId','beer_beerid', 'beer_name', 'beer_style','beer_abv','review_overall','review_time']]
+beer = beer[['userId','beer_beerid', 'beer_name','brewery_name','beer_style','beer_abv','review_overall','review_time']]
 
 
-# In[7]:
+# In[9]:
 
 
 #Sort so most recent reviews are first when users rated the same beer 2x
 beer =  beer.sort_values(by=['userId','beer_beerid', 'beer_name', 'beer_style','beer_abv','review_overall','review_time'], ascending = True)
 
 
-# In[8]:
+# In[10]:
 
 
 #Take first row where user reviewed same beer 2x..most current review will now be taken
 beer = beer.groupby(['userId','beer_beerid']).first().reset_index()
 
 
-# In[9]:
+# In[11]:
 
 
 #Identify beers that are given more than one unique ID and keep first
@@ -67,7 +67,7 @@ beer_names = beer_names.sort_values(by=['beer_name','beer_beerid'])
 beer_names = beer_names.loc[beer_names['beer_name'] != beer_names['beer_name'].shift()]
 
 
-# In[10]:
+# In[12]:
 
 
 #Join now unique ids back to original dataframe so beers only have 1 unique id
@@ -76,28 +76,28 @@ beer2 = beer2.drop(['beer_name_y'], axis=1)
 beer2 = beer2.rename(columns={'beer_name_x' :'beer_name'})
 
 
-# In[11]:
+# In[13]:
 
 
 #Filter out beers that are only rated once
 beer2 = beer2.groupby('beer_beerid').filter(lambda x: x['beer_beerid'].count()>1)
 
 
-# In[16]:
+# In[14]:
 
 
 #Write to csv for model and dictionary mapping
 beer2.to_csv('beer2.csv', sep=',')
 
 
-# In[16]:
+# In[15]:
 
 
 #Dataframe of unique values for heroku
 beer_uniques = beer2.drop_duplicates(subset=['beer_name'])
 
 
-# In[17]:
+# In[16]:
 
 
 #Write to csv - needed for reduced memory usage
